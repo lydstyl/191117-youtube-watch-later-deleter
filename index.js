77fdb4d98f6b9198382aecfe037c5212eb3222de
Gabriel Brun <lydstyl@gmail.com>
2019-11-22 14:03:24 +0100

    add the case of 1 button istead of 3 and refacto Stepdown Rule

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-//Paste this code in your console when you are in your Watch later list page if you whant to delete it
+// Paste all this code in your console when you are in your Watch later list page and press enter if you whant to delete it
 
-setInterval(() => {
+function clickItemButtonList() {
   document
     .querySelector('.style-scope ytd-playlist-video-renderer button')
     .click();
+}
 
+function inListButtonClick(buttonNumber) {
   document
-    .querySelectorAll('.style-scope ytd-menu-service-item-renderer')[2]
-    .click();
-}, 100);
+    .querySelectorAll('.style-scope ytd-menu-service-item-renderer')
+    [buttonNumber - 1].click();
+}
+
+function is3ButtonList() {
+  if (
+    document.querySelectorAll('.style-scope ytd-menu-service-item-renderer')
+      .length === 3
+  )
+    return true;
+  return false;
+}
+
+setInterval(() => {
+  clickItemButtonList();
+
+  is3ButtonList() ? inListButtonClick(3) : inListButtonClick(1);
+}, 200);
