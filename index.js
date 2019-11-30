2c697a40cec8126a8316bc5c2483499fea363435
Gabriel Brun <lydstyl@gmail.com>
2019-11-30 17:30:18 +0100

    add a comment

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function clickDeleteButton() {
     .querySelector(
       'path[d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"]'
     )
-    .parentElement.parentElement.parentElement.click();
+    .parentElement.parentElement.parentElement.click(); // a lot of parentElement but it is difficult to select the correct button without it
 }
 
 setInterval(() => {
